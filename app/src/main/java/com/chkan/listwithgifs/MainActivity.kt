bac39b8a70fc948d4ce9c142bba6d15689d30605
git-cdv <chkan.dv@gmail.com>
2021-10-28 15:05:27 +0300

    + UI List

--- app/src/main/java/com/chkan/listwithgifs/MainActivity.kt
+++ app/src/main/java/com/chkan/listwithgifs/MainActivity.kt
@@ -2,10 +2,27 @@ package com.chkan.listwithgifs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.chkan.listwithgifs.ui.listview.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ListViewModel by viewModels()
+    private var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState==null){
+            viewModel.getGifList()
+        }
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        navController = host.navController
     }
 }
